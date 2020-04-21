dd0c9392be7f0c087d65a3fccfbd275c76f05dcb
ericaholloway-hub <60237666+ericaholloway-hub@users.noreply.github.com>
2020-04-21 12:27:22 -0500

    Update main.py
    Write the code to add in a 4th test.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#Your name goes here
+#Erica Holloway
+#Test Averages GUI
 
-#Your algorithm should go here OR you should use comments throughout
+
 
 # This program uses a GUI to get three test
 # scores and display their average.
@@ -18,6 +19,7 @@ class TestAvg:
         self.test1_frame = tkinter.Frame(self.main_window)
         self.test2_frame = tkinter.Frame(self.main_window)
         self.test3_frame = tkinter.Frame(self.main_window)
+        self.test4_frame = tkinter.Frame(self.main_window)
         self.avg_frame = tkinter.Frame(self.main_window)
         self.button_frame = tkinter.Frame(self.main_window)
 
@@ -45,15 +47,27 @@ class TestAvg:
         self.test3_label.pack(side='left')
         self.test3_entry.pack(side='left')
 
+        # Create and pack the widgets for test 4.
+        self.test4_label = tkinter.Label(self.test4_frame,
+                                         text='Enter the score for test 4:')
+        self.test4_entry = tkinter.Entry(self.test4_frame,
+                                         width=10)
+        self.test4_label.pack(side='left')
+        self.test4_entry.pack(side='left')
+
         # Create and pack the widgets for the average.
         self.result_label = tkinter.Label(self.avg_frame,
                                           text='Average:')
         self.avg = tkinter.StringVar()  # To update avg_label
+        self.let = tkinter.StringVar()
         self.avg_label = tkinter.Label(self.avg_frame,
                                        textvariable=self.avg)
+        self.let_label = tkinter.Label(self.avg_frame,
+                                       textvariable=self.let)
 
         self.result_label.pack(side='left')
         self.avg_label.pack(side='left')
+        self.let_label.pack(side='left')
 
         # Create and pack the button widgets.
         self.calc_button = tkinter.Button(self.button_frame,
@@ -69,6 +83,7 @@ class TestAvg:
         self.test1_frame.pack()
         self.test2_frame.pack()
         self.test3_frame.pack()
+        self.test4_frame.pack()
         self.avg_frame.pack()
         self.button_frame.pack()
 
@@ -84,21 +99,28 @@ class TestAvg:
         self.test1 = float(self.test1_entry.get())
         self.test2 = float(self.test2_entry.get())
         self.test3 = float(self.test3_entry.get())
+        self.test4 = float(self.test4_entry.get())
 
         # Calculate the average.
         self.average = (self.test1 + self.test2 +
-                        self.test3) / 3.0
+                        self.test3 + self.test4) / 4.0
+
+        if self.average > 90:
+            self.letter= 'A'
+        elif self.average >= 80 and self.average <= 90:
+            self.letter= 'B'
+        elif self.average >= 70 and self.average <= 80:
+            self.letter= 'C'
+        else:
+            self.letter= 'F'
+        
 
 
         # Update the avg_label widget by storing
         # the value of self.average in the StringVar
         # object referenced by avg.
         self.avg.set(round(self.average,2))
+        self.let.set(self.letter) 
 
 # Create an instance of the TestAvg class.
 test_avg = TestAvg()
-
-
-
-
-
